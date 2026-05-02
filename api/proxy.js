dd488fd8be6ce04207d0b3167cfb88534d13cde1
SetupTV <96498736+ida-tv@users.noreply.github.com>
2026-05-02 20:31:52 +0300

    Add proxy handler for fetching external URLs

---
+++ api/proxy.js
@@ -0,0 +1,13 @@
+export default async function handler(req, res) {
+  const { url } = req.query;
+  if (!url) return res.status(400).send('No URL provided');
+  try {
+    const response = await fetch(url);
+    const data = await response.text();
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Content-Type', 'text/plain');
+    res.status(200).send(data);
+  } catch (error) {
+    res.status(500).send('Proxy error');
+  }
+}
